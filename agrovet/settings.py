acb697393bdac34617479f21ffe9ab8ba0515a23
Davila99 <davilaeliseo453@gmail.com>
2025-09-10 08:49:39 -0600

    Switch database from SQLite to PostgreSQL
    Updated the Django DATABASES setting to use PostgreSQL instead of SQLite, specifying database name, user, password, host, and port. This change enables the project to use a more robust and scalable database backend.

--- agrovet/settings.py
+++ agrovet/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'agrovet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sirelab1',            # Nombre de tu base de datos
+        'USER': 'postgres',        # Usuario creado en PostgreSQL
+        'PASSWORD': 'root',  # Contraseña del usuario
+        'HOST': 'localhost',       # O la IP/host del servidor
+        'PORT': '5432',            # Puerto por defecto de PostgreSQL
     }
 }
+
 
 
 # Password validation

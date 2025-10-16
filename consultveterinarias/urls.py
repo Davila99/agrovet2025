619730de2252aaa49dadf80514c3a3fd2380a578
Davila99 <davilaeliseo453@gmail.com>
2025-10-16 15:19:14 -0600

    integrando swagger
    integrando documentacion

--- consultveterinarias/urls.py
+++ consultveterinarias/urls.py
@@ -10,11 +10,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Mi API",
       default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
+      description="Documentación de mi API",
       license=openapi.License(name="BSD License"),
    ),
    public=True,
@@ -27,7 +25,8 @@ urlpatterns = [
     path('api/profiles/', include('profiles.api.urls')),
     path('api/auth/', include('auth_app.api.urls')),
     path('api/chat/', include('chat.api.urls')), 
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger<format>.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),  # ✅ Correcta
+    path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),          # ✅ Alternativa más usada
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]

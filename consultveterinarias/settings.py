aded1bd4be45abeb5d2b539f76d9720dfbaa2d41
eliseo <davilaeliseo453@gmail.com>
2025-11-12 15:20:39 -0600

    Update settings.py

--- consultveterinarias/settings.py
+++ consultveterinarias/settings.py
@@ -182,7 +182,8 @@ REST_FRAMEWORK = {
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",           # React local
-    "https://agrovets.vercel.app",    # deploy (sin / al final)
+    "https://agrovets.vercel.app", 
+    "https://shante-klephtic-nahla.ngrok-free.dev",# deploy (sin / al final)
 ]
 
 # Allow the Authorization header for cross-origin requests (useful in dev)
